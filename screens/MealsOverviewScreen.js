6d2c5c7a2008df84cf8e674cab4be3de59be903a
eduregis <eduregis1@gmail.com>
2024-08-16 19:31:40 -0300

    second page with styling

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,44 @@
+import { View, FlatList, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route }) {
+  const categoryId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
