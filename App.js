f642ee85833b32c2e3b3d62a38d73e1fd928e6f7
eduregis <eduregis1@gmail.com>
2024-08-17 16:44:54 -0300

    tabbar router

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+//import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 
-const Drawer = createDrawerNavigator();
+//const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      {/* <Drawer.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: "#3c0a6b" },
           headerTintColor: "white",
@@ -22,11 +26,48 @@ export default function App() {
           component={WelcomeScreen}
           options={{
             drawerLabel: "Welcome Screen",
-            //drawerIcon: ({ color }) =>
+            drawerIcon: ({ color, size }) => (
+              <Icon name="home" size={size} color={color} />
+            ),
           }}
         />
-        <Drawer.Screen name="User" component={UserScreen} />
-      </Drawer.Navigator>
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="user" size={size} color={color} />
+            ),
+          }}
+        />
+      </Drawer.Navigator> */}
+
+      <BottomTab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          tabBarActiveTintColor: "#3c0a6b",
+        }}
+      >
+        <BottomTab.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="user" size={size} color={color} />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
