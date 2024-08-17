f05a23702abea7b4bdcf210ce8064a866bdfafb5
eduregis <eduregis1@gmail.com>
2024-08-17 15:54:18 -0300

    styling side menu

--- App.js
+++ App.js
@@ -9,8 +9,22 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          drawerActiveBackgroundColor: "#f0e1ff",
+          drawerActiveTintColor: "#3c0a6b",
+        }}
+      >
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            //drawerIcon: ({ color }) =>
+          }}
+        />
         <Drawer.Screen name="User" component={UserScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
