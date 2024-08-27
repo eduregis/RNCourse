0c579b391f89dafb593debf8ac22659131f0f3b1
eduregis <eduregis1@gmail.com>
2024-08-27 20:33:02 -0300

    basic setup

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function PlaceForm() {
+  return (
+    <View>
+      <Text>PlaceForm</Text>
+    </View>
+  );
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({});
---
+++ components/UI/IconButton.js
@@ -0,0 +1,27 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
+
+const IconButton = ({ icon, size, color, onPress }) => {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <Icon name={icon} size={size} color={color} />
+    </Pressable>
+  );
+};
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
